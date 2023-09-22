c2683bc0872026275286d3268b34db26825e3868
Ayushi Vashisth <ayushivashisth22@gmail.com>
2023-09-22 15:06:53 +0530

    fix the code

--- src/App.js
+++ src/App.js
@@ -1,102 +1,11 @@
-// App.js
-import React, { useState } from "react";
-import "./App.css";
-import "./components/ButtomSheet.css";
-import ButtomSheet from "./components/ButtomSheet";
+import ButtomSheet from './components/ButtomSheet';
 
-const App = () => {
-  const [data, setData] = useState(null);
-
-  const openSheetFull = () => {
-    setData(570);
-  };
-  const openSheetHalf = () => {
-    setData(250);
-  };
-  const openSheetSmall = () => {
-    setData(150);
-  };
-  const close = () => {
-    setData(null);
-  };
-  const handleClose = () => {
-    setData(null);
-  };
+function App() {
   return (
     <>
-      <div className="screen">
-        {/* Close the sheet */}
-        <div
-          id="new-basic-menu"
-          data={data}
-          onClose={handleClose}
-          MenuListProps={{
-            "aria-labelledby": "basic-button"
-          }}
-        >
-          <div
-            className="new-card-details d-flex justify-content-center align-items-center"
-            style={{ width: "15rem", padding: 10, position: "relative" }}
-          >
-            <i
-              className="fas fa-close new-small-close"
-              onClick={handleClose}
-              style={{
-                position: "absolute",
-                fontSize: 23,
-                cursor: "pointer"
-              }}
-            ></i>
-          </div>
-        </div>
-        <div className="screen">
-          <h1 className="heading"> React Spring Bottom Sheet </h1>
-          {/* Small screen */}
-          <button
-            onClick={openSheetSmall}
-            style={{
-              gap: "10px",
-              marginRight: "20px",
-              padding: "10px",
-              fontSize: "20px",
-              marginBottom: "7px"
-            }}
-          >
-            Open Small Screen Sheet
-          </button>
-          {/* Medium screen button */}
-          <button
-            onClick={openSheetHalf}
-            style={{
-              gap: "10px",
-              marginRight: "20px",
-              padding: "10px",
-              fontSize: "20px",
-              marginBottom: "7px"
-            }}
-          >
-            Open Half Screen Sheet
-          </button>
-          {/* Full screen button */}
-          <button
-            onClick={openSheetFull}
-            style={{
-              gap: "10px",
-              marginRight: "20px",
-              padding: "10px",
-              fontSize: "20px",
-              marginBottom: "7px"
-            }}
-          >
-            {" "}
-            Open Full Screen Sheet
-          </button>
-        </div>
-
-        {data !== null && <ButtomSheet height={data} close={close} />}
-      </div>
+      <ButtomSheet/>
     </>
   );
-};
+}
 
 export default App;
--- src/components/ButtomSheet.jsx
+++ src/components/ButtomSheet.jsx
@@ -1,33 +1,87 @@
-import React from "react";
-import "./ButtomSheet.css";
+import { useEffect, useState } from "react";
 
-const ButtomSheet = ({ height, close }) => {
+export default function ButtomSheet() {
+  const [isDragging, setIsDragging] = useState(false);
+  const [customSheetHeight, setCustomSheetHeight] = useState(10);
+  const [startDragPosition, setStartDragPosition] = useState(0);
+
+  const getContactPosition = (event) =>
+    event.touches ? event.touches[0] : event;
+
+  const handleDragStart = (e) => {
+    setIsDragging(true);
+    const { pageY } = getContactPosition(e);
+    setStartDragPosition(pageY);
+  };
+
+  const handleDragMove = (event) => {
+    if (!startDragPosition) return;
+    const { pageY } = getContactPosition(event);
+    const deltaY = startDragPosition - pageY;
+    setStartDragPosition(pageY);
+    const deltaHeight = (deltaY / window.innerHeight) * 100;
+    setCustomSheetHeight((prevHeight) => prevHeight + deltaHeight);
+  };
+
+  const handleDragEnd = () => {
+    setStartDragPosition(0);
+    setIsDragging(false);
+    setCustomSheetHeight((prevHeight) => {
+      if (prevHeight < 25) {
+        return 10;
+      } else if (prevHeight > 75) {
+        return 90;
+      } else {
+        return 50;
+      }
+    });
+  };
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      if (isDragging) {
+        handleDragMove(event);
+      }
+    };
+
+    const handleMouseUp = () => {
+      if (isDragging) {
+        handleDragEnd();
+      }
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("mouseup", handleMouseUp);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, [isDragging, startDragPosition]);
+
   return (
-    // data which is seen on the different screens
-    <div className="new-sheet" style={{ height: `${height}px` }}>
-      <div className="new-sheet-1"></div>
-      <div className="new-sheet-content">
-        <h1>React sheet</h1>
-        <h4>
-          Built using React.js and various libraries like react-spring and
-          react-use-gesture. It follows best practices to minimize re-renders
-          and only animates CSS properties that can be GPU-accelerated when
-          possible.
-        </h4>
-        <p>
-          This component can function as a modal dialog that requires user
-          interaction before it can be closed, or as a floating bottom panel. It
-          dynamically adjusts to fit the dimensions of its content.
-        </p>
+    <main className={`flex min-h-screen flex-col items-center p-10 bg-gray-200`}>
+      <div
+        style={{ height: `${customSheetHeight}vh` }}
+        className={`fixed bottom-0 left-0 right-0 flex flex-col bg-black rounded-t-2xl max-w-5xl w-full mx-auto p-2 transition-all ease-in-out duration-300`}>
+        <div
+          onMouseDown={handleDragStart}
+          className={`w-full flex items-center justify-center p-2 cursor-pointer ${
+            isDragging ? "cursor-grabbing" : "cursor-grab"
+          }`}>
+          <div className={`w-8 h-2 bg-white rounded-full`}></div>
+        </div>
+        <div
+          className={`flex items-center justify-center text-lg sm:text-2xl font-mono py-4 my-1 h-full  text-white rounded-md bg-white`}>
+          <div className="flex flex-wrap">
+            <img src=""/>
+            <img src=""/>
+            <img src=""/>
+            <img src=""/>
+            <img src=""/>
+          </div>
+        </div>
       </div>
-      {/* sheet close */}
-      {height !== null && (
-        <button className="close-button" onClick={close}>
-          X
-        </button>
-      )}
-    </div>
+    </main>
   );
-};
-
-export default ButtomSheet;
+}
